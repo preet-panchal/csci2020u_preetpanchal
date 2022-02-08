6fc49a2ee7efebabd0d933d90683680197ba71aa
Preet Panchal <panchalpreet094@gmail.com>
2022-02-07 23:00:06 -0500

    final-lab03-commit

--- lab03/src/main/java/csci2020u/lab02/CSVDemo.java
+++ lab03/src/main/java/csci2020u/lab02/CSVDemo.java
@@ -1,8 +1,7 @@
-package org.apache.commons.csv;
+package csci2020u.lab02;
 
 import java.io.*;
 import org.apache.commons.csv.*;
-// import static org.apache.commons.csv.Token.Type.*;
 
 public class CSVDemo {
     public static void main(String[] args) {
